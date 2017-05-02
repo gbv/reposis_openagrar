10d1da790130bbf42ca339c7054c81ffa384fb22
Kathleen Neumann <neumann@gbv.de>
2017-05-02 19:54:59 +0000

    added sort option for modified added language EN added new menu item for "my documents"

--- save/webpages/js/openagrar.js
+++ save/webpages/js/openagrar.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function() {
 
+  // replace placeholder USERNAME with username
+  var userID = $("#currentUser strong").html();
+  var newHrefShort = 'https://www.openagrar.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='https://www.openagrar.de/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHrefShort);
+  var newHref = 'https://openagrar.bmel-forschung.de/servlets/solr/select?q=createdby:' + userID + '&fq=objectType:mods';
+  $("a[href='https://openagrar.bmel-forschung.de/servlets/solr/select?q=createdby:USERNAME']").attr('href', newHref);
+
+
 // spam protection for mails
   $('span.madress').each(function(i) {
       var text = $(this).text();
